24a2aabf9e89659ab6b8fdee3c94d97f31114ce6
Karlla Souzza <karlla.souzza7@gmail.com>
2026-02-17 14:24:19 -0300

    style(env): format error handling for environment variables
    - Improved readability of error handling code
    - Ensured consistent formatting for better maintainability

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,12 +3,19 @@ import { z } from "zod";
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "prod", "test"]).default("dev"),
+  DATABASE_URL: z
+    .string()
+    .default(
+      "postgresql://docker:docker@localhost:5433/apisolid?schema=public",
+    ),
   PORT: z.coerce.number().default(3000),
 });
 
 const _env = envSchema.safeParse(process.env);
 if (!_env.success) {
-  throw new Error("Invalid environment variables", { cause: _env.error.format() });
+  throw new Error("Invalid environment variables", {
+    cause: _env.error.format(),
+  });
 }
 
 export const env = _env.data;
