f013c410bdf502a214a47d045f56c775a7c78991
Karlla Souzza <karlla.souzza7@gmail.com>
2026-02-18 02:07:44 -0300

    feat(prisma): add vitest environment setup for Prisma
    - Create a new file for Prisma test environment setup
    - Implement database URL generation with schema
    - Add setup and teardown methods for database migrations

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import pg from "pg";
+
+import type { Environment } from "vitest/environments";
+
+const baseUrl = process.env.DATABASE_URL!;
+
+function generateDatabaseUrl(schema: string) {
+  if (!baseUrl) {
+    throw new Error("DATABASE_URL environment variable is not defined");
+  }
+
+  const url = new URL(baseUrl);
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
+
+export default <Environment>{
+  name: "prisma",
+  viteEnvironment: "ssr",
+  async setup(_global, _options) {
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync(`npx prisma migrate deploy`, {
+      stdio: "ignore",
+      env: {
+        ...process.env,
+        PRISMA_HIDE_UPDATE_MESSAGE: "true",
+      },
+    });
+
+    return {
+      async teardown() {
+        execSync(`npx prisma migrate reset --force`, {
+          stdio: "ignore",
+          env: {
+            ...process.env,
+            PRISMA_HIDE_UPDATE_MESSAGE: "true",
+          },
+        });
+      },
+    };
+  },
+};
